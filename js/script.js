40a19b989417eb37a398f7fcb78ccac44f5cd5b2
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-19 19:29:26 +0100

    Implemented index.html with two input tags, created a variable with the distance that user want to do

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Scrivere un programma che chieda all’utente:
+// Il numero di chilometri da percorrere
+// Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// il prezzo del biglietto è definito in base ai km (0.1976 € al km)
+// va applicato uno sconto del 17.65% per i minorenni
+// va applicato uno sconto del 53.27% per gli over 65.
+const userKilometers = parseInt (document.querySelector('input#kilometers').value)
+console.log(userKilometers)

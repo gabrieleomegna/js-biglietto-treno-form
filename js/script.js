3d1ea4942f69e777678c177d90f1daab8e21277a
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-20 09:56:30 +0100

    Implemented inner.html in script.js in order to write in page ticketPrice from js

--- js/script.js
+++ js/script.js
@@ -14,15 +14,16 @@ buttonElement.addEventListener ('click',
         // console.log(userKilometers);
         const userAge = parseFloat (document.querySelector('input#user-age').value);
         let ticketPrice = 0.1976 * userKilometers;
-        console.log('Il prezzo del biglietto è:', ticketPrice.toFixed(2));
+        let outputInPage = document.querySelector('div.output');
+        outputInPage.innerHTML ='Il prezzo del biglietto è:' + ticketPrice.toFixed(2);
         if (userAge < 18) {
             ticketPrice = ticketPrice - ((ticketPrice / 100) * 17.65);
-            console.log('Età minore di 18 anni, ticket ridotto:', ticketPrice.toFixed(2));
+            outputInPage.innerHTML += '<br>Età minore di 18 anni, ticket ridotto:' + ticketPrice.toFixed(2);
         } else if (userAge >= 65) {
             ticketPrice = ticketPrice - ((ticketPrice / 100) * 53.2);
-            console.log('Età maggiore di 65 anni, ticket ridotto:', ticketPrice.toFixed(2));
+            outputInPage.innerHTML += '<br>Età maggiore di 65 anni, ticket ridotto:' + ticketPrice.toFixed(2);
         } else {
-            console.log('Non sono applicabili riduzioni di prezzo')
+            outputInPage.innerHTML += '<br>Non sono applicabili riduzioni di prezzo';
         }
     }
 )

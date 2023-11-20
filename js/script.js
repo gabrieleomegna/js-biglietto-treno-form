23e38173c0839c7f2fc1c354d5f0405e21615ffd
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-20 09:29:15 +0100

    Added if condition in Event Listener, applied .toFixed method in ticketPrice

--- js/script.js
+++ js/script.js
@@ -13,6 +13,16 @@ buttonElement.addEventListener ('click',
         const userKilometers = parseFloat(document.querySelector('input#kilometers').value);
         // console.log(userKilometers);
         const userAge = parseFloat (document.querySelector('input#user-age').value);
-        
+        let ticketPrice = 0.1976 * userKilometers;
+        console.log('Il prezzo del biglietto è:', ticketPrice.toFixed(2));
+        if (userAge < 18) {
+            ticketPrice = ticketPrice - ((ticketPrice / 100) * 17.65);
+            console.log('Età minore di 18 anni, ticket ridotto:', ticketPrice.toFixed(2));
+        } else if (userAge >= 65) {
+            ticketPrice = ticketPrice - ((ticketPrice / 100) * 53.2);
+            console.log('Età maggiore di 65 anni, ticket ridotto:', ticketPrice.toFixed(2));
+        } else {
+            console.log('Non sono applicabili riduzioni di prezzo')
+        }
     }
 )
